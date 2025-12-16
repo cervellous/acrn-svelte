fce6a9a215a2698c5ecd36a813cbe5e4b3e8b6b7
dan <cervellous@gmail.com>
2025-12-16 16:48:10 -0600

    other svelte stuff

---
+++ acrn-svelte/vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'ACRN Protocol',
+        short_name: 'ACRN',
+        description: 'ACRN tinnitus treatment protocol',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon'
+          }
+        ]
+      }
+    }),
+    viteSingleFile()
+  ],
+  base: './',
+  build: {
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000
+  }
+})
